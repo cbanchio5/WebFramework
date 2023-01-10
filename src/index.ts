81c57fd29ac9c2a70a652279c6a36ce281f11bad
cesar banchio <cbanchio5@gmail.com>
2023-01-10 10:17:34 +0100

    added set and get methods to class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hi there')
+import { User } from "./models/User";
+
+
+const user = new User({name:'Cesar', age:30})
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+
+
+user.set({name:'Nico', age:78});
+
+
+console.log(user.get('name'));
+console.log(user.get('age'));
